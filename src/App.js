47610e6da3c492d9dfde6bc908638511aa3bc655
Afx31 <post4wjm@gmail.com>
2023-03-22 23:15:48 +1100

    Setup of open-meteo api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
+// fetch('https://api.open-meteo.com/v1/forecast?latitude=-33.79&longitude=150.83&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m')
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
 function App() {
+  const [temp, setTemp] = useState('');
+
+  useEffect(() => {
+    testfunction();
+  },[]);
+
+  const testfunction = async () => {
+    var res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=-33.79&longitude=150.83&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m')
+    var resData = await res.json();
+
+    console.log('----------')
+    console.log(resData['current_weather'])
+    console.log('----------')
+    setTemp(resData['current_weather'].temperature);
+  }
+
   return (
     <div className="App">
-      <p>Test</p>
+
+      <p>Temperature: {temp}</p>
     </div>
   );
 }
