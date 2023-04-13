141885f6d2cb256d24725b2eaa7ee5f093161b66
Afx31 <post4wjm@gmail.com>
2023-04-13 22:31:43 +1000

    Weekly forecast via search working, timezone is current hardcoded, need to fix

--- src/components/WeatherAPI/WeatherAPI.js
+++ src/components/WeatherAPI/WeatherAPI.js
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 import jsonData from './suburbs.json';
-//import jsonData from './suburbsmine.json';
 
 const WeatherAPI = () => {
   const [userInput, setUserInput] = useState('');
-  const [temp, setTemp] = useState('');
-  // maybe configurable later on for the user to search via different criteria
-  var searchBy = "suburb";
-  //var searchResults = [];
+  const [resTemp, setResTemp] = useState('');
+  const [weeklyForecast, setWeeklyForecast] = useState([]);
+  var searchBy = "suburb"; // make configurable later
 
   const APICall = async (long, lat) => {
     var res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`)
     var resData = await res.json();
-    setTemp(resData['current_weather'].temperature);
+    //console.log(resData)
+    setResTemp(resData['current_weather'].temperature);
+  }
+
+  const APICallDaily = async (long, lat) => {
+    //var res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&daily=temperature_2m_max,temperature_2m_min&timezone=Australia%2FSydney`);
+    var res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=-33.87&longitude=151.21&&daily=temperature_2m_max,temperature_2m_min&timezone=Australia%2FSydney`);
+    var resData = await res.json();
+    var tempArr = [];
+
+    for (var i = 0; i < resData['daily']['time'].length; i++) {
+      var dailyResult = {};
+      dailyResult.date = resData['daily']['time'][i];
+      dailyResult.tempMax = resData['daily']['temperature_2m_max'][i];
+      dailyResult.tempMin = resData['daily']['temperature_2m_min'][i];
+      tempArr.push(dailyResult);
+    }
+
+    setWeeklyForecast(tempArr);
   }
   
   const GetWeatherForecast = () => {
@@ -22,13 +38,13 @@ const WeatherAPI = () => {
     for (var i = 0; i < jsonData.data.length; i++) {
       if (jsonData.data[i][searchBy] == userInput) {
         //searchResults = jsonData.data[i];
-        console.log('Search found!!!')
         searchForLatitude = jsonData.data[i].lat;
         searchForLongitude = jsonData.data[i].long;
       }
     }
     
     APICall(searchForLongitude, searchForLatitude);
+    APICallDaily(searchForLongitude, searchForLatitude);
   }
 
   return (
@@ -36,7 +52,15 @@ const WeatherAPI = () => {
       <p>Your suburb: </p>
       <input id='suburbInput' type='text' name='suburbInput' onChange={e => setUserInput(e.target.value)} /><br/>
       <button type='button' onClick={GetWeatherForecast}>Get weather forecast</button>
-      <p>Temperature: {temp}</p>
+      <p>Current Temperature: {resTemp}</p>
+      <div>
+        <h1>Weekly forecast</h1>
+        {weeklyForecast.map((data) => (
+          <>
+            <p>{data.date} - {data.tempMax} / {data.tempMin}</p>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
